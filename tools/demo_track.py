0764cb50973e559e1828d098e4a7a97a6c75d660
Shuyang Sun <shuyangsun10@gmail.com>
2023-08-29 21:14:42 -0400

    Set default cuda device ID.

--- tools/demo_track.py
+++ tools/demo_track.py
@@ -317,6 +317,8 @@ def main(exp, args):
     else:
         device = args.device
     args.device = torch.device(device)
+    if args.device.startswith('cuda') and len(args.device.split(':')) > 1:
+        torch.cuda.set_device(int(args.device.split(':')[-1]))
 
     logger.info("Args: {}".format(args))
 
