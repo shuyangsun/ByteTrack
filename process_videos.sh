2be99b6493b89d65cce018d20ff0272cf7751b94
Shuyang Sun <shuyangsun10@gmail.com>
2023-08-29 23:58:24 -0400

    Script bug fix.

--- process_videos.sh
+++ process_videos.sh
@@ -15,14 +15,18 @@ file_list=("$directory_path"*".mp4")
 total_files=${#file_list[@]}
 start_position=$(echo "$total_files * $starting_position_ratio" | bc | awk '{print int($1)}')
 num_files_to_process=$(echo "$total_files * $fraction" | bc | awk '{print int($1)}')
+num_files_to_process_buf=$(echo "$num_files_to_process* 1.5" | bc | awk '{print int($1)}')
 
 # Ensure the last batch processes all remaining files with 50% buffer.
-if ((start_position + 1.5 * num_files_to_process > total_files)); then
+if ((start_position + num_files_to_process_buf > total_files)); then
     num_files_to_process=$((total_files - start_position))
 fi
+
+echo "Processing ${num_files_to_process} video files..."
 
 # Iterate through the selected files, starting from the calculated position, and process each one using the Python script
 for ((i = start_position; i < start_position + num_files_to_process && i < total_files; i++)); do
     file_name="${file_list[$i]}"
+    echo "Processing ${file_name}..."
     python tools/demo_track.py video -f exps/example/mot/yolox_x_ablation.py -c ${PATH_TO_BTRACK_MODEL} --fps 15 --fp16 --fuse --save_result --path ${file_name}
 done
